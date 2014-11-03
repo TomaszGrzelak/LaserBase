af8a3d60f66440f743f01eb8f357ce9d374bd553
Tomasz Grzelak <tomasz.grzelak@experteer.com>
2014-11-03 20:26:13 +0100

    Added Grunt

---
+++ Gruntfile.js
@@ -0,0 +1,42 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+
+    pkg: grunt.file.readJSON('package.json'),
+
+    concat: {
+      options: {
+        separator: "\n\n"
+      },
+      dist: {
+        src: [
+          'src/main.js'
+        ],
+        dest: 'build/<%= pkg.name %>.js'
+      }
+    },
+
+    mochaTest: {
+      test: {
+        src: ['test/**/*.js']
+      }
+    },
+
+    watch: {
+      files: ['<%%= jshint.files %>'],
+      tasks: ['concat', 'jshint', 'qunit']
+    }
+
+  });
+
+  // grunt.loadNpmTasks('grunt-contrib-uglify');
+  // grunt.loadNpmTasks('grunt-contrib-jshint');
+  // grunt.loadNpmTasks('grunt-contrib-qunit');
+  // grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-mocha-test');
+
+  grunt.registerTask('test', ['concat', 'mochaTest']);
+  grunt.registerTask('default', ['concat'])
+
+};
